10b08699c9f3e957d0fa6da02d65aaaae2f2e659
lucidbard <lucidbard@users.github.com>
2022-10-10 16:13:35 +0000

    Oct 10, 2022, 12:13 PM

--- Unity/Assets/SpawnOnStart.cs
+++ Unity/Assets/SpawnOnStart.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Ubiq.Spawning;
 using UnityEngine;
 
 public class SpawnOnStart : MonoBehaviour
 {
+
+    public GameObject objectPrefabToSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NetworkSpawnManager.Find(this).SpawnWithPeerScope(objectPrefabToSpawn);
     }
 
     // Update is called once per frame
